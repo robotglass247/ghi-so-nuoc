fb57eb09fac795ad5196bc77025d1a5de07d7bdb
robotglass247 <robotglass247@gmail.com>
2026-09-15 18:16:02 +0700

    Add Sheets API auth dev config

---
+++ 09_auth-sheets-config-dev.js
@@ -0,0 +1,8 @@
+/* DEV ONLY - Google Sheets API role auth config */
+window.WATER_AUTH_SHEETS_CONFIG=Object.freeze({
+  BUILD:'auth-sheets-config-dev-v1',
+  GOOGLE_CLIENT_ID:'617076693631-mc3c0kf70ngdd96jjnn6qk3g39jk2v6o.apps.googleusercontent.com',
+  AUTH_SHEET_ID:'13IM_nRlOBGAlb7n_PCmF3ZcbVKlR9koaTtstFeZl5qs',
+  AUTH_RANGE:'AUTH_NHAN_SU!A2:C1000',
+  OAUTH_SCOPE:'openid email https://www.googleapis.com/auth/spreadsheets.readonly'
+});
